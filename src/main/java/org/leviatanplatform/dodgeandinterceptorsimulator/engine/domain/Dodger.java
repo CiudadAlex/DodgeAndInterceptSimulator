3bca99c2ace812a999b57ef32466146e7679c6c4
CiudadAlex <ciudad.alex@gmail.com>
2026-05-26 00:36:02 +0200

    DodgeAndInterceptSimulator

--- src/main/java/org/leviatanplatform/dodgeandinterceptorsimulator/engine/domain/Dodger.java
+++ src/main/java/org/leviatanplatform/dodgeandinterceptorsimulator/engine/domain/Dodger.java
@@ -2,15 +2,48 @@ package org.leviatanplatform.dodgeandinterceptorsimulator.engine.domain;
 
 public class Dodger extends Projectile {
 
+    private static final double SQRT_2 = Math.sqrt(2);
+
     public Dodger(Position initialPosition, Velocity velocity, double radius) {
         super(initialPosition, velocity, radius);
     }
 
-    public void setInitialPosition(Position initialPosition) {
+    private void setInitialPosition(Position initialPosition) {
         this.initialPosition = initialPosition;
     }
 
-    public void setVelocity(Velocity velocity) {
+    private void setVelocity(Velocity velocity) {
         this.velocity = velocity;
+    }
+
+    public void move(Movement movement, double velocity) {
+
+        double vx = 0;
+        double vy = 0;
+
+        switch (movement) {
+            case UP -> vy = velocity;
+            case UP_RIGHT -> {
+                vx = SQRT_2 * velocity;
+                vy = SQRT_2 * velocity;
+            }
+            case RIGHT -> vx = velocity;
+            case DOWN_RIGHT -> {
+                vx = SQRT_2 * velocity;
+                vy = - SQRT_2 * velocity;
+            }
+            case DOWN -> vy = - velocity;
+            case DOWN_LEFT -> {
+                vx = - SQRT_2 * velocity;
+                vy = - SQRT_2 * velocity;
+            }
+            case LEFT -> vx = - velocity;
+            case UP_LEFT -> {
+                vx = - SQRT_2 * velocity;
+                vy = SQRT_2 * velocity;
+            }
+        }
+
+        setVelocity(new Velocity(vx, vy));
     }
 }
