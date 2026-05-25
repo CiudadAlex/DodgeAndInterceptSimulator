be6f804c24c89e54b06893c9ffaf84df3c4957e3
CiudadAlex <ciudad.alex@gmail.com>
2026-05-25 19:40:08 +0200

    DodgeAndInterceptSimulator

--- src/main/java/org/leviatanplatform/dodgeandinterceptorsimulator/engine/tools/ProjectileCollisionDetector.java
+++ src/main/java/org/leviatanplatform/dodgeandinterceptorsimulator/engine/tools/ProjectileCollisionDetector.java
@@ -1,12 +1,37 @@
 package org.leviatanplatform.dodgeandinterceptorsimulator.engine.tools;
 
+import org.leviatanplatform.dodgeandinterceptorsimulator.engine.domain.Position;
 import org.leviatanplatform.dodgeandinterceptorsimulator.engine.domain.Projectile;
 
 public class ProjectileCollisionDetector {
 
-    public static boolean isThereCollision(Projectile p1, Projectile p2, double timeInitial, double timeFinal, int precision) {
+    public static boolean isThereCollision(Projectile projectile1, Projectile projectile2, double timeInitial, double timeFinal, int precision) {
 
-        // FIXME finish
+        double sumRadius = projectile1.getRadius() + projectile2.getRadius();
+        double timeStep = (timeFinal - timeInitial) / precision;
+
+        for (int i = 0; i <= precision; i++) {
+            double time = timeInitial + i * timeStep;
+            Position position1 = projectile1.getPosition(time);
+            Position position2 = projectile2.getPosition(time);
+            double distance = calculateDistance(position1, position2);
+
+            if (distance < sumRadius) {
+                return true;
+            }
+        }
+
         return false;
+    }
+
+    public static double calculateDistance(Position position1, Position position2) {
+
+        double x1 = position1.getX();
+        double y1 = position1.getY();
+
+        double x2 = position2.getX();
+        double y2 = position2.getY();
+
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 }
